3b7ae7109e1e4cef144c7f6f0ae034f22550a3be
samuelhoffmann1 <104939060+samuelhoffmann1@users.noreply.github.com>
2022-05-11 21:08:37 -0700

    Create Beetle.py

---
+++ Beetle Folder/Beetle.py
@@ -0,0 +1,12 @@
+class beetle:
+    def __init__(self, num):
+        self.name = input("Player {} Name? ".format(num))
+        self.body = 0
+        self.head = 0
+        self.legs = 0
+        self.eyes = 0
+        self.antenna = 0
+        self.wings = 0
+        self.turns = 0
+
+
